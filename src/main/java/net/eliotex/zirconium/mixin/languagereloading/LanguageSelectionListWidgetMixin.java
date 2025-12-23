0210af91791c7b5c229e04ac465ea38971123c41
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2025-12-23 17:02:49 +0000

    Hopefully

---
+++ src/main/java/net/eliotex/zirconium/mixin/languagereloading/LanguageSelectionListWidgetMixin.java
@@ -0,0 +1,22 @@
+package net.eliotex.zirconium.mixin.languagereloading;
+
+import net.minecraft.client.Minecraft;
+import net.minecaft.client.gui.screen.options.LanguageOptionsScreen.LanguageSelectionListWidget
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin({LanguageSelectionListWidget.class})
+public class LanguageSelectionListWidgetMixin {
+   @WrapOperation(
+      method = {"entryClicked"},
+      at = {@At(
+   value = "INVOKE",
+   target = "Lnet/minecraft/client/Minecraft;reloadResources()V"
+)}
+   )
+   private void onlyReloadLanguage(Minecraft client, Operation<Void> original) {
+      client.getLanguageManager().reload(client.getResourceManager());
+   }
+}
