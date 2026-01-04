34b0c3c7fa4a1b24fd4e4e08dba0255191b743bd
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-01-04 01:49:13 +0000

    Attempt to make it run

--- src/main/java/net/eliotex/zirconium/mixin/fontbatching/TextRendererAccessor.java
+++ src/main/java/net/eliotex/zirconium/mixin/fontbatching/TextRendererAccessor.java
@@ -1,8 +1,12 @@
 package net.eliotex.zirconium.mixin.fontbatching;
 
 import net.eliotex.zirconium.utils.font.BatchingTextRenderer;
+import net.minecraft.client.render.TextRenderer;
 import net.minecraft.resource.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
+@Mixin(TextRenderer.class)
 public interface TextRendererAccessor {
 
     int angelica$drawStringBatched(String text, int x, int y, int argb, boolean dropShadow);
