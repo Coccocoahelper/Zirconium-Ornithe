280c84675c8bc7ce2c09e42d265301139e6c1018
thatonecoder (formerly Coccocoa's Helper) <157546848+Coccocoahelper@users.noreply.github.com>
2026-01-23 02:14:53 +0000

    bruh

--- src/main/java/net/eliotex/zirconium/mixin/fasterglint/ItemRendererMixin.java
+++ src/main/java/net/eliotex/zirconium/mixin/fasterglint/ItemRendererMixin.java
@@ -31,31 +31,28 @@ public abstract class ItemRendererMixin {
 
         GLX.blendFuncSeparate(772, 1, 0, 1);
 
-        //for(int layer = 0; layer < 2; ++layer) {
-            final int timeUVDenominator = 3000 /*+ layer * 1873*/;
-            final float timeUVNoise = (float)(time % (long)timeUVDenominator) / (float)timeUVDenominator * 256F;
+        final int timeUVDenominator = 3000;
+        final float timeUVNoise = (float)(time % (long)timeUVDenominator) / (float)timeUVDenominator * 256F;
 
-            tessellator.startDrawingQuads();
-            tessellator.addVertexWithUV(
-                 posX, (posY + height), zLevel,
-                 ((timeUVNoise + (float)height * layerUVNoise) * timeUVSpeed), ((float)height * timeUVSpeed)
-            );
-            tessellator.addVertexWithUV(
-                (posX + width), (posY + height), zLevel,
-                ((timeUVNoise + (float)width + (float)height * layerUVNoise) * timeUVSpeed), ((float)height * timeUVSpeed)
-            );
-            tessellator.addVertexWithUV(
-                 (posX + width), posY, zLevel,
-                 ((timeUVNoise + (float)width) * timeUVSpeed), 0D
-            );
-            tessellator.addVertexWithUV(
-                 posX, posY, zLevel,
-                 (timeUVNoise * timeUVSpeed), 0D
-            );
-            tessellator.draw();
-
-        //layerUVNoise = -1.0F;
-        //}
+        // TODO: Recode these calls, as they were removed in 1.8
+        tessellator.startDrawingQuads();
+        tessellator.addVertexWithUV(
+            posX, (posY + height), zLevel,
+            ((timeUVNoise + (float)height * layerUVNoise) * timeUVSpeed), ((float)height * timeUVSpeed)
+        );
+        tessellator.addVertexWithUV(
+            (posX + width), (posY + height), zLevel,
+            ((timeUVNoise + (float)width + (float)height * layerUVNoise) * timeUVSpeed), ((float)height * timeUVSpeed)
+        );
+        tessellator.addVertexWithUV(
+            (posX + width), posY, zLevel,
+            ((timeUVNoise + (float)width) * timeUVSpeed), 0D
+        );
+        tessellator.addVertexWithUV(
+            posX, posY, zLevel,
+            (timeUVNoise * timeUVSpeed), 0D
+        );
+        tessellator.draw();
 
         GLX.blendFuncSeparate(770, 771, 1, 0);
     }
